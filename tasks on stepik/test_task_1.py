0cbf377e87e63d0873e891eb5c6b5d3c30f4b472
Igor <ferumcarbon@mail.ru>
2024-11-15 15:29:56 +0300

    init

---
+++ tasks on stepik/test_task_1.py
@@ -0,0 +1,28 @@
+alfabet_e = 'abcdefghijklmnopqrstuvwxyz'
+
+
+def request_data():
+    message = input('Введите ваше сообщение:_')
+    word_breakdown(message)
+
+
+def word_breakdown(text):
+    for elem in text.split():
+        key = len(elem.strip('''!'?,".'''))
+        encrypt(elem, key)
+
+
+def encrypt(text, key):
+    encrypted_text = ''
+    for c in text:
+        if c.isalpha() and c == c.lower():
+            encrypted_text += alfabet_e[(alfabet_e.index(c) + key) % len(alfabet_e)]
+        elif c.isalpha() and c == c.upper():
+            encrypted_text += alfabet_e[(alfabet_e.index(c.lower()) + key) % len(alfabet_e)].upper()
+        else:
+            encrypted_text += c
+
+    print(encrypted_text, end=' ')
+
+
+request_data()
